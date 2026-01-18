b786aa08d10ff73ada3ce8247e1a00a675eacf06
Codrix <codrix.ai@gmail.com>
2026-01-18 14:24:25 -0300

    -feat: Agregadas las materias Paradigmas y Discreta II, ademas de excalidraw y algunas actualizaciones en sistemas operativos

---
+++ paradigmas/experimentals/token_error.c
@@ -0,0 +1,12 @@
+/*
+    Error Léxico (EN C): 
+    int 9x para el scanner:
+
+        int = IDENTIFICADOR (válido)
+
+        9 = (válido si termina hasta ahí)
+        
+        9x = (Inválido)
+
+*/
+int 9x;
